d0206fd84e1a2bdc876c34b3f87e5181770493fa
Sebastian <sebastian.clausen@hotmail.com>
2021-03-25 14:14:28 +0100

    Initial commit

---
+++ Lommeregner2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using LommeregnerBibliotek;
+
+namespace Lommeregner2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bool der viser om lommeregneren kører
+            bool appAktiv = true;
+
+            //Opret en lommeregner klasse
+            Lommeregner lommeregner = new Lommeregner();
+
+            while (appAktiv)
+            {
+                //Variabler der bruges til beregning
+                string input1 = "";
+                string input2 = "";
+                double result = 0;
+
+                //Spørg bruger efter det første tal
+                Console.WriteLine("Indtast det første tal og tryk på enter");
+                input1 = Console.ReadLine();
+
+                double cleanNum1 = 0;
+                while (!double.TryParse(input1, out cleanNum1))
+                {
+                    Console.WriteLine("Ikke gyldigt input, venligst indtast et tal!");
+                    input1 = Console.ReadLine();
+                }
+
+                //Spørg bruger efter det andet tal
+                Console.WriteLine("Indtast det andet tal og tryk på enter");
+                input2 = Console.ReadLine();
+
+                double cleanNum2 = 0;
+                while (!double.TryParse(input2, out cleanNum2))
+                {
+                    Console.WriteLine("Ikke gyldigt input, venligst indtast et tal!");
+                    input1 = Console.ReadLine();
+                }
+
+                //Spørg brugeren efter en operator!
+                Console.WriteLine("Vælg en operator fra listen:");
+                Console.WriteLine("\tp - Plus");
+                Console.WriteLine("\tm - Minus");
+                Console.WriteLine("\tg - Gange");
+                Console.WriteLine("\td - Dividere");
+                Console.Write("Dit Valg? ");
+
+                string op = Console.ReadLine();
+                try
+                {
+                    result = lommeregner.LavBeregning(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("Denne operation vil føre til en matematisk fejl!\n");
+                    }
+                    else
+                    {
+                        //{0:0.##} sætter to tegn efter decimalet
+                        Console.WriteLine("Resultatet er: {0:0.##}\n", result);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Åh nej! Der skete en fejl\n - Fejlkode: " + e.Message);
+                }
+                Console.WriteLine("------------------------\n");
+                Console.WriteLine("Tryk på 'e' også enter for at afslutte lommeregneren...\n" +
+                "Tryk på en anden knap end 'e' også enter for at genstarte lommeregneren...");
+                if (Console.ReadLine() == "e")
+                {
+                    appAktiv = false;
+                }
+            }
+            //Skriver det sidste til JSON objektet(end array og end object og lukker writeren)
+            lommeregner.Finish();
+            return;
+        }
+    }
+}
---
+++ LommeregnerBibliotek/Class1.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LommeregnerBibliotek
+{
+    public class Lommeregner
+    {
+        //Opretter en JSON writer, denne bruges til at skrive i JSON format.
+        JsonWriter writer;
+        //Constructor der opretter en streawriter
+        public Lommeregner()
+        {
+            //Opretter streamwriteren
+            StreamWriter logFil = File.CreateText("lommeregnerlog.json");
+            logFil.AutoFlush = true;
+            writer = new JsonTextWriter(logFil);
+            writer.Formatting = Formatting.Indented;
+            //Skriver starten på et JSON object
+            writer.WriteStartObject();
+            //Skriver property navnet
+            writer.WritePropertyName("Operationer");
+            //Skriver begyndelsen på et JSON array
+            writer.WriteStartArray();
+
+        }
+
+        //Num1 er det første tal, num2 er det andet tal, op er operatoren(+,-,/,*)
+        public double LavBeregning(double num1, double num2, string op)
+        {
+            //Sættes default som Not a Number i tilfælde af brugeren indtaster noget "ulovligt"
+            double result = double.NaN;
+            //Skriver starten på et JSON objekt
+            writer.WriteStartObject();
+            //Skriver operand1
+            writer.WritePropertyName("Operand1");
+            //Skriver tal 1
+            writer.WriteValue(num1);
+            //Skriver operand2
+            writer.WritePropertyName("Operand2");
+            //Skriver tal 2
+            writer.WriteValue(num2);
+            //Skriver operator
+            writer.WritePropertyName("Operator");
+            switch (op)
+            {
+                case "p":
+                    result = num1 + num2;
+                    //Skriver plus
+                    writer.WriteValue("Plus");
+                    break;
+                case "m":
+                    result = num1 - num2;
+                    //Skriver minus
+                    writer.WriteValue("Minus");
+                    break;
+                case "g":
+                    result = num1 * num2;
+                    //Skriver gange
+                    writer.WriteValue("Gange"); break;
+                case "d":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        //Skriver division
+                        writer.WriteValue("Division");
+                    }
+                    break;
+                default:
+                    break;
+            }
+            //Skriver resultat
+            writer.WritePropertyName("Resultat");
+            //Skriver værdien af resultat
+            writer.WriteValue(result);
+            //Lukker JSON objektet
+            writer.WriteEndObject();
+            return result;
+        }
+
+        //Metode til at skrive slutningen af JSON formattet
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+    }
+}
